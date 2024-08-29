231df282ea915414341e015033ab22c71f5780b2
Mark Newman <mark@trinetteandmark.com>
2024-08-28 21:32:20 -0400

    Delete ReimageWindows/choco_install.ps1

--- ReimageWindows/choco_install.ps1
+++
@@ -1,7 +0,0 @@
-if('Unrestricted' -ne (Get-ExecutionPolicy)) { Set-ExecutionPolicy Bypass -Scope Process -Force }
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-refreshenv
-
-choco install "7zip;adobereader;notepadplusplus;tailscale" -y
-choco install "git;vscode;docker-desktop" -y
-choco install "aescrypt;filezilla" -y
